3e6e13500053f225f455950e2ae9982de85c8085
Seu Nome <seuemail@example.com>
2025-02-20 22:38:11 -0300

    retornando variavel

--- src/main/java/com/fatec/projeto/projeto2025/Controllers/ExercicioController.java
+++ src/main/java/com/fatec/projeto/projeto2025/Controllers/ExercicioController.java
@@ -2,13 +2,14 @@ package com.fatec.projeto.projeto2025.Controllers;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RestController
 
 public class ExercicioController {
 
-    @GetMapping("") 
-    public String HelloWorld() {
-        return "hello";
+    @GetMapping("{nome}") 
+    public String HelloWorld(@PathVariable String nome) {
+        return nome;
     }
 }
